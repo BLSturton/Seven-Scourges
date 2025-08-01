b59bd0a4de277c37d908a0343a9f5f76e4b4f35c
BLSturton <51524832+BLSturton@users.noreply.github.com>
2025-08-01 13:22:08 -0500

    CetusWalk Done, Starting Dialouge

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,28 +2,67 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    //General Variables
+    //Move
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
     Vector2 movement;
+
+    //Animation
+    public Animator CetusAnim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        CetusAnim.SetBool("Idle", true);
     }
     // Update is called once per frame
     void Update()
     {
-        //Input
+        //This section detects movement by input, and then plays the animations depending on the input.
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        if(movement == new Vector2(0, 0))
+        {
+            CetusAnim.SetBool("Idle", true);
+            CetusAnim.SetBool("WalkDown", false);
+            CetusAnim.SetBool("WalkUp", false);
+            CetusAnim.SetBool("WalkLeft", false);
+            CetusAnim.SetBool("WalkRight", false);
+        }
+        if(movement.y == -1) 
+        {
+            CetusAnim.SetBool("Idle", false);
+            CetusAnim.SetBool("WalkDown", true);
+            CetusAnim.SetBool("WalkLeft", false);
+            CetusAnim.SetBool("WalkRight", false);
+        }
+        if (movement.y == 1)
+        {
+            CetusAnim.SetBool("Idle", false);
+            CetusAnim.SetBool("WalkUp", true);
+            CetusAnim.SetBool("WalkLeft", false);
+            CetusAnim.SetBool("WalkRight", false);
+        }
+        if (movement.x == -1)
+        {
+            CetusAnim.SetBool("Idle", false);
+            CetusAnim.SetBool("WalkLeft", true);
+            CetusAnim.SetBool("WalkDown", false);
+            CetusAnim.SetBool("WalkUp", false);
+        }
+        if (movement.x == 1)
+        {
+            CetusAnim.SetBool("Idle", false);
+            CetusAnim.SetBool("WalkRight", true);
+            CetusAnim.SetBool("WalkDown", false);
+            CetusAnim.SetBool("WalkUp", false);
+        }
     }
 
     private void FixedUpdate()
     {
         //Move
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
     }
 }
