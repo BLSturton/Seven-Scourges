f132f70f35fdef2e5353116196d083fddce97c86
BLSturton <51524832+BLSturton@users.noreply.github.com>
2025-08-06 17:05:20 -0500

    Background Switch

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BattleManager : MonoBehaviour
 {
@@ -6,14 +7,27 @@ public class BattleManager : MonoBehaviour
     [SerializeField] public GameObject[] partyList;
     //1 For Cetus, 2 for Ratic
     [SerializeField] public int currentPartyTurn;
+    [SerializeField] public bool enemyTurn;
 
     [SerializeField] OptionPicker cetusOptionPicker;
     [SerializeField] OptionPicker raticOptionPicker;
+
+    [SerializeField] GameObject playerBackground;
+    [SerializeField] GameObject playerObjects;
+    [SerializeField] GameObject enemyBackground;
+    [SerializeField] GameObject enemyObjects;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentPartyTurn = 1;
         cetusOptionPicker.myTurn = true;
+
+        playerBackground.SetActive(true);
+        playerObjects.SetActive(true);
+        enemyBackground.SetActive(false);
+        enemyObjects.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,13 +35,32 @@ public class BattleManager : MonoBehaviour
     {
         if (cetusOptionPicker.endTurn) 
         {
-            raticOptionPicker.myTurn = true;
             
+            if(partyList.Length > 1) 
+            {
+                raticOptionPicker.myTurn = true;
+            }
+            else 
+            {
+              enemyTurn = true;
+
+            }
         }
         if (raticOptionPicker.endTurn) 
         {
             raticOptionPicker.myTurn = false;
-            Debug.Log("enemy timeeeeeee");
+            enemyTurn = true;
+
+        }
+
+        //Starts enemy turn
+        if (enemyTurn) 
+        {
+            playerBackground.SetActive(false);
+            playerObjects.SetActive(false);
+            enemyBackground.SetActive(true);
+            enemyObjects.SetActive(true);
+
         }
     }
 }
