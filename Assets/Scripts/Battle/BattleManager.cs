7e34e135d2b2caad61b54acbbe49f3b96df33f69
BLSturton <51524832+BLSturton@users.noreply.github.com>
2025-08-05 23:30:48 -0500

    PlayerTurnBones

--- Assets/Scripts/Battle/BattleManager.cs
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] public GameObject[] enemyList;
+    [SerializeField] public GameObject[] partyList;
+    //1 For Cetus, 2 for Ratic
+    [SerializeField] public int currentPartyTurn;
 
+    [SerializeField] OptionPicker cetusOptionPicker;
+    [SerializeField] OptionPicker raticOptionPicker;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentPartyTurn = 1;
+        cetusOptionPicker.myTurn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (cetusOptionPicker.endTurn) 
+        {
+            raticOptionPicker.myTurn = true;
+            
+        }
+        if (raticOptionPicker.endTurn) 
+        {
+            raticOptionPicker.myTurn = false;
+            Debug.Log("enemy timeeeeeee");
+        }
     }
 }
--- Assets/Scripts/Battle/OptionPicker.cs
+++ Assets/Scripts/Battle/OptionPicker.cs
@@ -21,6 +21,10 @@ public class OptionPicker : MonoBehaviour
     [SerializeField] public int enemyNumber;
     [SerializeField] public int maxEnemy;
     [SerializeField] public GameObject enemyPicker;
+
+    //End turn
+    [SerializeField] public bool endTurn;
+    [SerializeField] public bool myTurn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -35,144 +39,173 @@ public class OptionPicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Advance Option
-        if (Input.GetKeyDown(KeyCode.D) && !selectCooldown && !actionPicked)
+        if (myTurn) 
         {
-            if (optionNumber == 3)
-            {
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
-                selectedOption = attackOptions[0];
-                optionNumber = 0;
-                selectCooldown = true;
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
-                StartCoroutine(AttackWait());
-            }
-            else
-            {
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
-                selectedOption = attackOptions[optionNumber + 1];
-                optionNumber++;
-                selectCooldown = true;
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
-
-                StartCoroutine(AttackWait());
-            }
-
-        }
-        //Reverse Option
-        if (Input.GetKeyDown(KeyCode.A) && !selectCooldown && !actionPicked)
-        {
-            if (optionNumber == 0)
-            {
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
-                selectedOption = attackOptions[3];
-                optionNumber = 3;
-                selectCooldown = true;
-                StartCoroutine(AttackWait());
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
-
-            }
-            else
-            {
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
-                selectedOption = attackOptions[optionNumber - 1];
-                optionNumber--;
-                selectCooldown = true;
-                StartCoroutine(AttackWait());
-                selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
-
-            }
-
-        }
-
-        //Confirm Option
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            //If attacking
-            if (attackOn)
-            {
-                Debug.Log("Cetus is attacking" + battleManager.enemyList[enemyNumber]);
-                attackOn = false;
-                enemyPicker.SetActive(false);
-            }
-            if (!actionPicked) 
-            {
-                switch (optionNumber)
-                {
-                    case 0:
-                        attackOn = true;
-                        break;
-                }
-            }
-           
             foreach (GameObject obj in attackOptions)
             {
                 if (obj != null)
                 {
-                    obj.SetActive(false); // Deactivates the GameObject
+                    obj.SetActive(true); // Deactivates the GameObject
                 }
             }
-            actionPicked = true;
-           
-        }
-        if (attackOn)
-        {
-            enemyPicker.SetActive(true);
             //Advance Option
-            if (Input.GetKeyDown(KeyCode.D) && !selectCooldown)
+            if (Input.GetKeyDown(KeyCode.D) && !selectCooldown && !actionPicked)
             {
-                if (enemyNumber == maxEnemy)
+                if (optionNumber == 3)
                 {
-
-                    selectedOption = battleManager.enemyList[0];
-                    enemyNumber = 0;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
+                    selectedOption = attackOptions[0];
+                    optionNumber = 0;
                     selectCooldown = true;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
                     StartCoroutine(AttackWait());
                 }
                 else
                 {
-                    selectedOption = battleManager.enemyList[enemyNumber + 1];
-                    enemyNumber++;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
+                    selectedOption = attackOptions[optionNumber + 1];
+                    optionNumber++;
                     selectCooldown = true;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
 
                     StartCoroutine(AttackWait());
                 }
 
             }
             //Reverse Option
-            if (Input.GetKeyDown(KeyCode.A) && !selectCooldown)
+            if (Input.GetKeyDown(KeyCode.A) && !selectCooldown && !actionPicked)
             {
-                if (enemyNumber == 0)
+                if (optionNumber == 0)
                 {
-
-                    selectedOption = battleManager.enemyList[maxEnemy];
-                    enemyNumber = 2;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
+                    selectedOption = attackOptions[3];
+                    optionNumber = 3;
                     selectCooldown = true;
                     StartCoroutine(AttackWait());
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
+
                 }
                 else
                 {
-                    selectedOption = battleManager.enemyList[enemyNumber - 1];
-                    enemyNumber--;
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y - 1);
+                    selectedOption = attackOptions[optionNumber - 1];
+                    optionNumber--;
                     selectCooldown = true;
-
                     StartCoroutine(AttackWait());
+                    selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
+
                 }
 
             }
 
-            //Moves selector
-            switch (enemyNumber) 
+            //Confirm Option
+            if (Input.GetKeyDown(KeyCode.K))
             {
-                case 0:
-                    enemyPicker.transform.position = new Vector2(4.75f, .2f);
-                    break;
-                case 1:
-                    enemyPicker.transform.position = new Vector2(6f, -1.2f);
-                    break;
-                case 2:
-                    enemyPicker.transform.position = new Vector2(4.5f, -2.5f);
-                    break;
+                foreach (GameObject obj in attackOptions)
+                {
+                    if (obj != null)
+                    {
+                        obj.SetActive(false); // Deactivates the GameObject
+                    }
+                }
+                //If attacking
+                if (attackOn)
+                {
+
+                    attackOn = false;
+                    enemyPicker.SetActive(false);
+                    endTurn = true;
+                    myTurn = false;
+                }
+                if (!actionPicked)
+                {
+                    switch (optionNumber)
+                    {
+                        case 0:
+                            attackOn = true;
+                            break;
+                    }
+                }
+
+                foreach (GameObject obj in attackOptions)
+                {
+                    if (obj != null)
+                    {
+                        obj.SetActive(false); // Deactivates the GameObject
+                    }
+                }
+                actionPicked = true;
+
+            }
+            if (attackOn)
+            {
+                enemyPicker.SetActive(true);
+                //Advance Option
+                if (Input.GetKeyDown(KeyCode.D) && !selectCooldown)
+                {
+                    if (enemyNumber == maxEnemy)
+                    {
+
+                        selectedOption = battleManager.enemyList[0];
+                        enemyNumber = 0;
+                        selectCooldown = true;
+                        StartCoroutine(AttackWait());
+                    }
+                    else
+                    {
+                        selectedOption = battleManager.enemyList[enemyNumber + 1];
+                        enemyNumber++;
+                        selectCooldown = true;
+
+                        StartCoroutine(AttackWait());
+                    }
+
+                }
+                //Reverse Option
+                if (Input.GetKeyDown(KeyCode.A) && !selectCooldown)
+                {
+                    if (enemyNumber == 0)
+                    {
+
+                        selectedOption = battleManager.enemyList[maxEnemy];
+                        enemyNumber = 2;
+                        selectCooldown = true;
+                        StartCoroutine(AttackWait());
+                    }
+                    else
+                    {
+                        selectedOption = battleManager.enemyList[enemyNumber - 1];
+                        enemyNumber--;
+                        selectCooldown = true;
+
+                        StartCoroutine(AttackWait());
+                    }
+
+                }
+
+                //Moves selector
+                switch (enemyNumber)
+                {
+                    case 0:
+                        enemyPicker.transform.position = new Vector2(4.75f, .2f);
+                        break;
+                    case 1:
+                        enemyPicker.transform.position = new Vector2(6f, -1.2f);
+                        break;
+                    case 2:
+                        enemyPicker.transform.position = new Vector2(4.5f, -2.5f);
+                        break;
+                }
+            }
+        }
+        else 
+        {
+            foreach (GameObject obj in attackOptions)
+            {
+                if (obj != null)
+                {
+                    obj.SetActive(false); // Deactivates the GameObject
+                }
             }
         }
     }
@@ -182,11 +215,6 @@ public class OptionPicker : MonoBehaviour
         yield return new WaitForSeconds(.2f);
         selectCooldown = false;
     }
-    //Starts a new turn by having attack selected
-    public void newTurn() 
-    {
-        selectedOption = attackOptions[0];
-        optionNumber = 0;
-        selectedOption.transform.localPosition = new Vector2(selectedOption.transform.localPosition.x, selectedOption.transform.localPosition.y + 1);
-    }
+    //Starts a new turn after an option is given
+   
 }
